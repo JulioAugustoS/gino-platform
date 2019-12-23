4d9cdbfaf19c494db67ae2805acdda4bf99cf3a1
Julio Augusto <julioams@icloud.com>
2019-12-23 01:31:12 -0300

    init created scenary

---
+++ Assets/Scripts/MoveBg.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBg : MonoBehaviour {
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 offset = new Vector2(Time.time * speed, 0f);
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
